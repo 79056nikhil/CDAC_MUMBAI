2d60e8739ee927c521b64c7d9d2e8d406697f329
NIKHIL_J <141175706+79056nikhil@users.noreply.github.com>
2025-03-07 08:40:33 +0530

    Que 2 completed

---
+++ OOPJAVA/ASS_01/Que02.java
@@ -0,0 +1,8 @@
+public class Que02 {
+    public static void main(String[] args) {
+        int num = 50;
+        double converted = num; // Automatic conversion
+        System.out.println("Int value: " + num);
+        System.out.println("Double value: " + converted);
+    }
+}
