66e6a9dfb3fa75a3b73fa2dc7ad7f5fbf56f220b
NIKHIL_J <141175706+79056nikhil@users.noreply.github.com>
2025-03-07 08:43:24 +0530

    Que 5 completed

---
+++ OOPJAVA/ASS_01/Que05.java
@@ -0,0 +1,14 @@
+public class Que05 {
+    public static void main(String[] args) {
+        int binary = 0b1010; // Binary literal
+        int octal = 012; // Octal literal
+        int hex = 0xA; // Hexadecimal literal
+        double scientific = 1.23e2; // Scientific notation
+
+        System.out.println("Binary: " + binary);
+        System.out.println("Octal: " + octal);
+        System.out.println("Hexadecimal: " + hex);
+        System.out.println("Scientific Notation: " + scientific);
+    }
+
+}
