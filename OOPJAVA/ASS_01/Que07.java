ecb0414139b5272f62adba70e853f1db26e02aa7
NIKHIL_J <141175706+79056nikhil@users.noreply.github.com>
2025-03-07 08:44:46 +0530

    Que 7 completed

---
+++ OOPJAVA/ASS_01/Que07.java
@@ -0,0 +1,11 @@
+public class Que07 {
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) {
+            System.out.println("Flag is true!");
+        } else {
+            System.out.println("Flag is false!");
+        }
+    }
+
+}
